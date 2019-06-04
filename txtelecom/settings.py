26fdaa51b8bbe2d73425757a30ac24127326e65a
juliobsilva <jooble111@gmail.com>
2019-06-04 13:13:59 -0300

    Ajustando settings

--- txtelecom/settings.py
+++ txtelecom/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -128,5 +128,4 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
